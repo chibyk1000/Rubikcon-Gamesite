dd1edff0d18e142c43981bca9a7afaecc538e42b
CleanScripts100 <88936315+chibyk1000@users.noreply.github.com>
2025-11-14 19:08:43 +0100

    updated

---
+++ client/dist/assets/all-wallets-C36qBODA.js
@@ -0,0 +1,6 @@
+import{b as e}from"./core-DDy-ede0.js";import"./index-eUlIsDch.js";import"./events-xBLQbFbj.js";import"./index.es-DLgkATHc.js";const o=e`<svg fill="none" viewBox="0 0 24 24">
+  <path
+    style="fill: var(--wui-color-accent-100);"
+    d="M10.2 6.6a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM21 6.6a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM10.2 17.4a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM21 17.4a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0Z"
+  />
+</svg>`;export{o as allWalletsSvg};
--- client/dist/assets/chevron-bottom-CNLOv_6U.js
+++
@@ -1,8 +0,0 @@
-import{b as e}from"./core-Bp-myBbR.js";import"./index-BBXRAfOT.js";import"./events-D0kWp7RH.js";import"./index.es-DVwrfZEP.js";const o=e`<svg fill="none" viewBox="0 0 16 16">
-  <path
-    fill="currentColor"
-    fill-rule="evenodd"
-    d="M1.46 4.96a1 1 0 0 1 1.41 0L8 10.09l5.13-5.13a1 1 0 1 1 1.41 1.41l-5.83 5.84a1 1 0 0 1-1.42 0L1.46 6.37a1 1 0 0 1 0-1.41Z"
-    clip-rule="evenodd"
-  />
-</svg>`;export{o as chevronBottomSvg};
